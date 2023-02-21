31a801f75518e11ebe2793211d6a46b20d2cc485
sneharajpurkar <snehasrajpurkar@gmail.com>
2023-02-21 09:40:50 +0530

    todo

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+
 function addTodo(){
-    var gettingTodos = document.getElementById('todosHere').value;
+    var gettingTodos = document.getElementById('todohere').value;
     if (gettingTodos) {
         var myTodosFromLS = JSON.parse(localStorage.getItem("Todos")) || [];
         myTodosFromLS.push(gettingTodos);
         localStorage.setItem("Todos", JSON.stringify(myTodosFromLS));
-        document.getElementById('todosHere').value = " ";
-        alert("Todo Added !")
-    } else {
-        alert('Add todo first, before submitting !')
+        document.getElementById('todohere').value = " ";
+        alert("Todo added !");
+    } else{
+        alert('Add todo first, before submitting!');
     }
 }
