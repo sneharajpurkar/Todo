84f40872f744227d8728bc2327b4a58da665c2ba
sneharajpurkar <snehasrajpurkar@gmail.com>
2023-02-21 19:31:36 +0530

    todo

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 
-function addTodo(){
+function addTodo() {
     var gettingTodos = document.getElementById('todohere').value;
     if (gettingTodos) {
         var myTodosFromLS = JSON.parse(localStorage.getItem("Todos")) || [];
         myTodosFromLS.push(gettingTodos);
         localStorage.setItem("Todos", JSON.stringify(myTodosFromLS));
         document.getElementById('todohere').value = " ";
+        cs();
         alert("Todo added !");
-    } else{
+    } else {
         alert('Add todo first, before submitting!');
     }
+}
+function cs() {
+    var divFromHtml = document.getElementById("displayTodos")
+    var gettingTodosFromLS = JSON.parse(localStorage.getItem("Todos"));
+    console.log(gettingTodosFromLS, "gettingTodosFromLS")
+    var finalStructure = []
+
+    for (var i = 0; i < gettingTodosFromLS.length; i++) {
+        finalStructure += `<div id="row">
+        <p>${gettingTodosFromLS[i]}</p>
+        <i onclick="deleteTodo(${i})" class="fa-solid fa-trash"></i>
+        </div>`
+    }
+    divFromHtml.innerHTML = finalStructure;
+}
+cs();
+function deleteTodo(todoIndex){
+    console.log(todoIndex);
+    var todoFromLS = JSON.parse(localStorage.getItem("Todos"));
+    todoFromLS.splice(todoIndex, 1)
+    localStorage.setItem("Todos", JSON.stringify(todoFromLS))
+    cs();
 }
